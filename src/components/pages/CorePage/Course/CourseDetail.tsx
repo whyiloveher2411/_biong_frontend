c7697bff1f7c030097f6f36ceeb7721d1f219d50
ken <quandang@poccupine.com>
2022-09-09 10:05:04 +0700

    fix error scroll to tab content

--- src/components/pages/CorePage/Course/CourseDetail.tsx
+++ src/components/pages/CorePage/Course/CourseDetail.tsx
@@ -21,9 +21,6 @@ const useStyles = makeCSS({
         '& .MuiTabs-flexContainer': {
             justifyContent: 'center',
         },
-        '& .tabContent': {
-            marginTop: 64,
-        }
     }
 });
 
