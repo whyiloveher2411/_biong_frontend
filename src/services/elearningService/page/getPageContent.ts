c52519274550d4c4cf7522f9f758966fb1b8a6b1
ken <quandang@poccupine.com>
2022-10-15 15:54:01 +0700

    cache page static

--- src/services/elearningService/page/getPageContent.ts
+++ src/services/elearningService/page/getPageContent.ts
@@ -1,22 +1,23 @@
+import cacheWindow from 'hook/cacheWindow';
 import { ajax } from 'hook/useApi';
 import { PageContentProps } from '../@type';
 
 export default async (group: string, slugPost: string): Promise<PageContentProps | null> => {
+    return cacheWindow('vn4-e-learning/page/get-content/' + group + '/' + slugPost, async () => {
+        let api = await ajax<{
+            post: PageContentProps,
+        }>({
+            url: 'vn4-e-learning/page/get-content',
+            data: {
+                group: group,
+                post: slugPost,
+            }
+        });
 
-    let api = await ajax<{
-        post: PageContentProps,
-    }>({
-        url: 'vn4-e-learning/page/get-content',
-        data: {
-            group: group,
-            post: slugPost,
+        if (api.post) {
+            return api.post;
         }
+
+        return null;
     });
-
-    if (api.post) {
-        return api.post;
-    }
-
-    return null;
-
 }
--- src/services/elearningService/page/getPagesOfGroup.ts
+++ src/services/elearningService/page/getPagesOfGroup.ts
@@ -1,21 +1,26 @@
+import cacheWindow from 'hook/cacheWindow';
 import { ajax } from 'hook/useApi';
 import { PageContentProps } from '../@type';
 
 export default async (group: string): Promise<PageContentProps[] | null> => {
 
-    let api = await ajax<{
-        pages: PageContentProps[],
-    }>({
-        url: 'vn4-e-learning/page/get-page-of-group',
-        data: {
-            group: group,
+    return cacheWindow('vn4-e-learning/page/get-page-of-group/' + group, async () => {
+
+        let api = await ajax<{
+            pages: PageContentProps[],
+        }>({
+            url: 'vn4-e-learning/page/get-page-of-group',
+            data: {
+                group: group,
+            }
+        });
+
+        if (api.pages) {
+            return api.pages;
         }
-    });
 
-    if (api.pages) {
-        return api.pages;
-    }
+        return null;
+    })
 
-    return null;
 
 }
