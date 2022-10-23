01d500777329330527903b24781d8aaf22d8a098
ken <quandang@poccupine.com>
2022-10-23 22:34:17 +0700

    update banner loading

--- src/components/molecules/Banner.tsx
+++ src/components/molecules/Banner.tsx
@@ -127,72 +127,90 @@ export function BannerLoading(props: {
     return (
         <Box
             component='section'
-            sx={{
-                display: 'flex',
-                position: 'relative',
-                alignItems: 'center',
-                zIndex: 1,
-            }}
+            sx={
+                (theme) => ({
+                    display: 'flex',
+                    position: 'relative',
+                    zIndex: 1,
+                    [theme.breakpoints.down('md')]: {
+                        flexDirection: 'column-reverse',
+                        alignItems: 'flex-end',
+                    }
+                })
+            }
         >
             <Box
                 sx={{
-                    width: '48%',
+                    width: '50%',
                     flexShrink: 0,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
                     zIndex: 1,
+                    [theme.breakpoints.down('md')]: {
+                        width: '100%',
+                        marginTop: '-66px',
+                    }
                 }}
 
             >
-                {
-                    props.children ?
-                        props.children
-                        :
-                        <>
-                            <Skeleton variant='rectangular'>
-                                <Typography sx={{
-                                    mt: 3, fontWeight: 500, fontSize: 14, textTransform: 'uppercase', letterSpacing: '0.5px',
-                                    '&:after': {
-                                        content: "''",
-                                        display: 'block',
+                <Box>
+                    {
+                        props.children ?
+                            props.children
+                            :
+                            <>
+                                <Skeleton variant='rectangular'>
+                                    <Typography sx={{
+                                        mt: 3, fontWeight: 500, fontSize: 14, textTransform: 'uppercase', letterSpacing: '0.5px',
+                                        '&:after': {
+                                            content: "''",
+                                            display: 'block',
+                                            height: '2px',
+                                            marginTop: '16px',
+                                            width: '80px',
+                                        }
+                                    }}>Lorem ipsum, dolor</Typography>
+                                </Skeleton>
+                                <Skeleton variant='rectangular'
+                                    sx={{
                                         height: '2px',
                                         marginTop: '16px',
                                         width: '80px',
-                                    }
-                                }}>Lorem ipsum, dolor</Typography>
-                            </Skeleton>
-                            <Skeleton variant='rectangular'
-                                sx={{
-                                    height: '2px',
-                                    marginTop: '16px',
-                                    width: '80px',
-                                }}
-                            />
+                                    }}
+                                />
 
-                            <Skeleton>
-                                <Typography sx={{ mt: 2, lineHeight: '56px', letterSpacing: '-0.5px', fontSize: 48, fontWeight: 400 }} variant='h1' component='h2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
-                            </Skeleton>
-                            <Skeleton>
-                                <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
-                            </Skeleton>
-                            <Skeleton>
-                                <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
-                            </Skeleton>
-                            <Skeleton >
-                                <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
-                            </Skeleton>
-                        </>
-                }
+                                <Skeleton>
+                                    <Typography sx={{ mt: 2, lineHeight: '56px', letterSpacing: '-0.5px', fontSize: 48, fontWeight: 400 }} variant='h1' component='h2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
+                                </Skeleton>
+                                <Skeleton>
+                                    <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
+                                </Skeleton>
+                                <Skeleton>
+                                    <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
+                                </Skeleton>
+                                <Skeleton >
+                                    <Typography sx={{ mt: 1, lineHeight: '28px', fontSize: 18 }} variant='subtitle1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit</Typography>
+                                </Skeleton>
+                            </>
+                    }
+                </Box>
             </Box>
             <Box
                 sx={{
-                    width: '100%',
                     position: 'relative',
                     zIndex: 0,
+                    display: 'block',
+                    width: '50%',
+                    [theme.breakpoints.down('md')]: {
+                        width: '72%',
+                    }
                 }}
             >
                 <Box
                     sx={{
                         position: 'absolute',
-                        left: '-41.5%',
+                        left: '-42.5%',
                         width: '83.2%',
                         zIndex: 1,
                     }}
@@ -201,14 +219,27 @@ export function BannerLoading(props: {
                         <path fillRule="evenodd" clipRule="evenodd" d="M0 0H553L285 159.314L0 0Z" fill={theme.palette.divider}></path>
                     </svg>
                 </Box>
-                <Skeleton
-                    variant='rectangular'
+                <Box
                     sx={{
+                        overflow: 'hidden',
                         width: '100%',
-                        minHeight: 506,
-                        clipPath: 'polygon(-10% 0,100% 0,100% 100%,26% 100%)',
+                        position: 'relative',
+                        paddingBottom: '83.09%',
+                        height: '100%',
                     }}
-                />
+                >
+                    <Skeleton
+                        variant='rectangular'
+                        sx={{
+                            clipPath: 'polygon(-10% 0,100% 0,100% 100%,26% 100%)',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            height: '100%',
+                            width: '100%',
+                        }}
+                    />
+                </Box>
             </Box>
         </Box >
     )
