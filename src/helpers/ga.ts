ee40bd1f00c522c5e0b50447d582cff04b28c4e4
ken <quandang@poccupine.com>
2022-11-30 12:06:06 +0700

    fix error gtag not function

--- src/helpers/ga.ts
+++ src/helpers/ga.ts
@@ -1,8 +1,8 @@
 export function gaEventPageView(time = 1000) {
     setTimeout(() => {
-        window.gtag('event', 'page_view', {
+        window.gtag ? window.gtag('event', 'page_view', {
             page_title: document.title,
             page_location: window.location.href,
-        });
+        }) : '';
     }, time);
 }
