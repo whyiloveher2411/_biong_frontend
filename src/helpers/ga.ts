7af1c340d47962a8bc2c01847abbc52d440457f6
ken <quandang@poccupine.com>
2022-11-30 13:57:58 +0700

    fix error gtag

--- src/helpers/ga.ts
+++ src/helpers/ga.ts
@@ -1,8 +1,10 @@
 export function gaEventPageView(time = 1000) {
     setTimeout(() => {
-        window.gtag ? window.gtag('event', 'page_view', {
-            page_title: document.title,
-            page_location: window.location.href,
-        }) : '';
+        if (window.gtag) {
+            window.gtag('event', 'page_view', {
+                page_title: document.title,
+                page_location: window.location.href,
+            })
+        }
     }, time);
 }
