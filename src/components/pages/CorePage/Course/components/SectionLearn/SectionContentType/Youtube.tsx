522b9b089953cb5d886f3389592e8feaea7b1aa9
ken <quandang@poccupine.com>
2022-12-18 17:02:36 +0700

    remove border iframe

--- src/components/pages/CorePage/Course/components/SectionLearn/SectionContentType/Youtube.tsx
+++ src/components/pages/CorePage/Course/components/SectionLearn/SectionContentType/Youtube.tsx
@@ -243,6 +243,7 @@ function Youtube({ lesson, process, style, handleAutoCompleteLesson }: {
                             left: 0,
                             width: '100%',
                             height: '100%',
+                            border: 'none',
                         }}
                         allowFullScreen
                     ></iframe>
