096f7cc88c6250a105b89116bfd3ce3d4c0cb481
ken <quandang@poccupine.com>
2023-03-02 14:05:57 +0700

    fix error discount info

--- src/components/organisms/DiscountInfo.tsx
+++ src/components/organisms/DiscountInfo.tsx
@@ -15,16 +15,33 @@ function DiscountInfo({
     children?: React.ReactElement,
 }) {
 
+    const [discountInfo, setDiscountInfo] = React.useState<CourseProps['discount_info'] | null>(null)
+
     const precent_discount = Number(Number(100 - (Number(course.price) * 100 / (Number(course.compare_price) ?? 1))).toFixed(1));
 
-    if (precent_discount > 0 && course.discount_info) {
+    React.useEffect(() => {
+        let discount_info = course.discount_info;
+
+        if (typeof discount_info === 'string') {
+            try {
+                discount_info = JSON.parse(discount_info);
+            } catch (error) {
+                discount_info = null;
+            }
+        }
+
+        setDiscountInfo(discount_info);
+
+    }, [course]);
+
+    if (precent_discount > 0 && discountInfo) {
         return (<TooltipWhite
             title={<>
-                <Typography variant='h5'>{course.discount_info.title}&nbsp;&nbsp;<Chip component='span' color='error' size='small' label={'- ' + precentFormat(precent_discount ?? 0)} />
+                <Typography variant='h5'>{discountInfo.title}&nbsp;&nbsp;<Chip component='span' color='error' size='small' label={'- ' + precentFormat(precent_discount ?? 0)} />
                 </Typography>
-                <Typography variant='body2' sx={{ mt: 1, fontSize: 14, lineHeight: '24px' }}>Chương trình khuyến mãi áp dụng từ ngày {dateFormat(course.discount_info.start_time, 'DD-MM-YYYY')} đến hết ngày {dateFormat(course.discount_info.end_time, 'DD-MM-YYYY')}, khóa học {course.title} giá gốc <Typography component='span' sx={{ color: 'error.main', display: 'inline-block', }}>{moneyFormat(course.compare_price)}</Typography> còn <Typography component='span' sx={{ color: 'success.main', display: 'inline-block', }}>{moneyFormat(course.price)}</Typography>
+                <Typography variant='body2' sx={{ mt: 1, fontSize: 14, lineHeight: '24px' }}>Chương trình khuyến mãi áp dụng từ ngày {dateFormat(discountInfo.start_time, 'DD-MM-YYYY')} đến hết ngày {dateFormat(discountInfo.end_time, 'DD-MM-YYYY')}, khóa học {course.title} giá gốc <Typography component='span' sx={{ color: 'error.main', display: 'inline-block', }}>{moneyFormat(course.compare_price)}</Typography> còn <Typography component='span' sx={{ color: 'success.main', display: 'inline-block', }}>{moneyFormat(course.price)}</Typography>
                     {
-                        course.discount_info.note ?
+                        discountInfo.note ?
                             <Box
                                 component="span"
                                 sx={{
@@ -32,7 +49,7 @@ function DiscountInfo({
                                         margin: 0,
                                     }
                                 }}
-                                dangerouslySetInnerHTML={{ __html: course.discount_info.note }}
+                                dangerouslySetInnerHTML={{ __html: discountInfo.note }}
                             />
                             : null
                     }
@@ -51,7 +68,7 @@ function DiscountInfo({
                             ...sx,
                         }}
                         // label="Đang khuyến mãi"
-                        label={course.discount_info.title + ' - ' + precentFormat(precent_discount)}
+                        label={discountInfo.title + ' - ' + precentFormat(precent_discount)}
                     />
             }
         </TooltipWhite>)
