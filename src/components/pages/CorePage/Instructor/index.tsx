b808cb5daa8fced9fb217de43933a8a7f7d9f6cb
ken <quandang@poccupine.com>
2023-03-06 14:28:02 +0700

    func overview perfoman

--- src/components/pages/CorePage/Instructor/index.tsx
+++ src/components/pages/CorePage/Instructor/index.tsx
@@ -64,7 +64,7 @@ function index() {
 
     return (
         <Page
-            title={title !== '...' ? title + ' - Instructor' : '...'}
+            title={title !== 'Instructor' ? title + ' - Instructor' : 'Instructor'}
             description='Bạn có thắc mắc hay cần báo cáo vấn đề xảy ra với sản phẩm hoặc dịch vụ của Spacedev.vn? Chúng tôi luôn sẵn sàng hỗ trợ bạn.'
             image='https://spacedev.vn/images/share-fb-540x282-2.jpg'
             maxWidth='100%'
--- src/components/pages/CorePage/Instructor/pages/Performance/Overview.tsx
+++ src/components/pages/CorePage/Instructor/pages/Performance/Overview.tsx
@@ -1,16 +1,19 @@
-import { Button } from "@mui/material";
-import Icon from "components/atoms/Icon";
-import MoreButton from "components/atoms/MoreButton";
-import Typography from "components/atoms/Typography";
-import useQuery from "hook/useQuery";
-import useCourse from "../../useCourse";
-import CardContent from "components/atoms/CardContent";
+import { Box, Grid } from "@mui/material";
 import Card from "components/atoms/Card";
+import CardContent from "components/atoms/CardContent";
+import Typography from "components/atoms/Typography";
+import useAjax from "hook/useApi";
+import useQuery from "hook/useQuery";
+import React from "react";
+import useCourse from "../../useCourse";
 
-function Overview() {
+function Overview({ setTitle }: { setTitle: (title: string) => void }) {
 
     const { courses } = useCourse();
 
+    const useApi = useAjax();
+
+    const dataOverview = React.useState<{ [key: string]: number }>({});
 
     const urlParam = useQuery({
         course: 0,
@@ -25,6 +28,21 @@ function Overview() {
             })
         },
     })) : [];
+
+    React.useEffect(() => {
+
+        useApi.ajax({
+            url: 'vn4-e-learning/instructor/performance/overview',
+            success: (result: { data: { [key: string]: number } }) => {
+                if (result.data) {
+                    dataOverview[1](result.data);
+                } else {
+                    dataOverview[1]({});
+                }
+            }
+        });
+        setTitle('Performance Overview');
+    }, []);
 
 
     let indexCourseSelected = -1;
@@ -59,31 +77,59 @@ function Overview() {
             }
         }}>
             Tổng quan
-            <MoreButton
-                actions={[listSelectCourse]}
-            >
-                <Button
-                    endIcon={<Icon icon="ExpandMoreOutlined" />}
-                    sx={{
-                        fontSize: 'inherit',
-                        padding: 0,
-                        marginLeft: 3,
-                        textTransform: 'initial',
-                        color: 'inherit',
-                        lineHeight: 'unset',
-                    }}
-                >
-                    {
-                        listSelectCourse[indexCourseSelected]?.title ?? '...'
-                    }
-                </Button>
-            </MoreButton>
         </Typography>
-        <Card>
-            <CardContent>
-                <Typography variant="h3">Tiến độ học tập</Typography>
-            </CardContent>
-        </Card>
+        <Grid
+            container
+            spacing={6}
+        >
+            {
+                courses?.map(item => (
+                    <Grid
+                        key={item.id}
+                        item
+                        md={4}
+                        xs={12}
+                    >
+                        <Card>
+                            <CardContent
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                }}
+                            >
+                                <Typography variant="h3">{item.title}</Typography>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        gap: 2,
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                        }}
+                                    >
+                                        <Typography variant="overline">Đã đăng ký</Typography>
+                                        <Typography variant="h4">{dataOverview[0]['register_' + item.id] ?? 0}</Typography>
+                                    </Box>
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                        }}
+                                    >
+                                        <Typography variant="overline">Học thử</Typography>
+                                        <Typography variant="h4">{dataOverview[0]['trial_' + item.id] ?? 0}</Typography>
+                                    </Box>
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))
+            }
+        </Grid>
+
     </>)
 }
 
