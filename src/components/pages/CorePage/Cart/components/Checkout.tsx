2270fc6d66641978d79f00daebd365a34a75df6d
ken <quandang@poccupine.com>
2023-03-07 14:35:36 +0700

    optimize messge checkout

--- src/components/pages/CorePage/Cart/components/Checkout.tsx
+++ src/components/pages/CorePage/Cart/components/Checkout.tsx
@@ -109,8 +109,6 @@ function Checkout({ courses, total }: {
             >
                 <Alert color='warning' sx={{ mt: 1, fontSize: 14, }} icon={false}>
                     <Typography>Sau khi thanh toán, hãy bấm nút "Xác nhận thanh toán", đơn hàng của bạn sẽ được xử lý trong vòng 2h đến 24h. </Typography>
-                </Alert>
-                <Alert color='info' sx={{ mt: 1, fontSize: 14, }} icon={false}>
                     <Typography>Nếu bạn không thể tìm thấy phương thức thanh toán phù hợp, bạn có thể liên hệ với chúng tôi qua fanpage <Typography sx={{ color: 'primary.main' }} component={MuiLink} href="https://www.facebook.com/spacedev.vn" target='_blank'>https://www.facebook.com/spacedev.vn</Typography> hoặc số điện thoại 0886871094 (Quân) để được hướng đẫn các phương thức khác</Typography>
                 </Alert>
             </Box>
