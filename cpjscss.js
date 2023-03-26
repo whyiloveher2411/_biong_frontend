c341fc8a71e80b35a99a6fd62c8911363e5d9ef7
ken <quandang@poccupine.com>
2023-03-26 18:07:32 +0700

    test

--- cpjscss.js
+++ cpjscss.js
@@ -4,7 +4,34 @@ const filePath = './build/asset-manifest.json';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) throw err;
-    const jsfile = (JSON.parse(data)).entrypoints[0];
+    const cssfile = (JSON.parse(data)).entrypoints[0];
+
+
+    const sourceFilePath = './build/'+cssfile;
+    const destinationFilePath = './build/static/css/main.css';
+
+    // create a read stream from the source file
+    const readStream = fs.createReadStream(sourceFilePath);
+
+    // create a write stream to the destination file
+    const writeStream = fs.createWriteStream(destinationFilePath);
+
+    // pipe the read stream to the write stream to copy the file
+    readStream.pipe(writeStream);
+
+    // listen for the 'finish' event to know when the copy operation has completed
+    writeStream.on('finish', () => {
+        console.log('File copied css successfully!');
+    });
+
+});
+
+
+
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const jsfile = (JSON.parse(data)).entrypoints[1];
 
 
     const sourceFilePath = './build/'+jsfile;
@@ -21,7 +48,10 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 
     // listen for the 'finish' event to know when the copy operation has completed
     writeStream.on('finish', () => {
-        console.log('File copied successfully!');
+        console.log('File copied javascript successfully!');
     });
 
 });
+
+
+
