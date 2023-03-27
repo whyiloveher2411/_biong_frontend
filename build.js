0c6933d0f43031a8bdf5e2701fb2332d50a2fb8f
ken <quandang@poccupine.com>
2023-03-27 14:05:38 +0700

    test optimize build

--- build.js
+++ build.js
@@ -2,6 +2,11 @@
 const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/build.js');
 const config = defaults.__get__('config');
+
+
 config.optimization.splitChunks = {
     chunks: 'all',
+    maxInitialRequests: Infinity,
+    minSize: 10000,
+    maxSize: 300000,
 };
