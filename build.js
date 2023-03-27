9e341cead603b28c0fa1a44be49a942016000021
ken <quandang@poccupine.com>
2023-03-27 12:56:26 +0700

    optimize build chunk

---
+++ build.js
@@ -0,0 +1,7 @@
+// in ./build.js
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/build.js');
+const config = defaults.__get__('config');
+config.optimization.splitChunks = {
+    chunks: 'all',
+};
