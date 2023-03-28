e9f1dd0c195c696bfdead7674cc136f4190a9535
ken <quandang@poccupine.com>
2023-03-28 22:16:01 +0700

    fix error load icon

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,8 +1,235 @@
+import BookmarksOutlined from '@mui/icons-material/BookmarksOutlined';
+import Brightness2Outlined from '@mui/icons-material/Brightness2Outlined';
+import CardGiftcardRounded from '@mui/icons-material/CardGiftcardRounded';
+import CheckRounded from '@mui/icons-material/CheckRounded';
+import EqualizerRounded from '@mui/icons-material/EqualizerRounded';
+import FeedOutlined from '@mui/icons-material/FeedOutlined';
+import FileDownloadOutlined from '@mui/icons-material/FileDownloadOutlined';
+import GitHub from '@mui/icons-material/GitHub';
+import HistoryOutlined from '@mui/icons-material/HistoryOutlined';
+import HistoryRounded from '@mui/icons-material/HistoryRounded';
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import MessageOutlined from '@mui/icons-material/MessageOutlined';
+import MoreHorizRounded from '@mui/icons-material/MoreHorizRounded';
+import MoreVert from '@mui/icons-material/MoreVert';
+import OndemandVideoOutlined from '@mui/icons-material/OndemandVideoOutlined';
+import OutlinedFlagRounded from '@mui/icons-material/OutlinedFlagRounded';
+import PriorityHighRounded from '@mui/icons-material/PriorityHighRounded';
+import ReportGmailerrorredRounded from '@mui/icons-material/ReportGmailerrorredRounded';
+import SchoolOutlined from '@mui/icons-material/SchoolOutlined';
+import Star from '@mui/icons-material/Star';
+import Twitter from '@mui/icons-material/Twitter';
+import YouTube from '@mui/icons-material/YouTube';
 import SvgIcon from 'components/atoms/SvgIcon';
-import React from 'react'
+import React from 'react';
 
-export type IconFormat = string | {
-    custom: string
+import AccessTimeOutlined from '@mui/icons-material/AccessTimeOutlined';
+import AccountTreeOutlined from '@mui/icons-material/AccountTreeOutlined';
+import Add from '@mui/icons-material/Add';
+import AddRounded from '@mui/icons-material/AddRounded';
+import AddToPhotosRounded from '@mui/icons-material/AddToPhotosRounded';
+import Apple from '@mui/icons-material/Apple';
+import ArrowBackOutlined from '@mui/icons-material/ArrowBackOutlined';
+import ArrowBackRounded from '@mui/icons-material/ArrowBackRounded';
+import ArrowDownwardRounded from '@mui/icons-material/ArrowDownwardRounded';
+import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
+import ArrowDropDownOutlined from '@mui/icons-material/ArrowDropDownOutlined';
+import ArrowDropUp from '@mui/icons-material/ArrowDropUp';
+import ArrowDropUpOutlined from '@mui/icons-material/ArrowDropUpOutlined';
+import ArrowForwardRounded from '@mui/icons-material/ArrowForwardRounded';
+import ArrowRight from '@mui/icons-material/ArrowRight';
+import ArrowUpwardRounded from '@mui/icons-material/ArrowUpwardRounded';
+import AssignmentIndOutlined from '@mui/icons-material/AssignmentIndOutlined';
+import AutoAwesomeMosaicOutlined from '@mui/icons-material/AutoAwesomeMosaicOutlined';
+import AutoFixHigh from '@mui/icons-material/AutoFixHigh';
+import Bookmark from '@mui/icons-material/Bookmark';
+import BookmarkBorder from '@mui/icons-material/BookmarkBorder';
+import Cached from '@mui/icons-material/Cached';
+import CardGiftcardOutlined from '@mui/icons-material/CardGiftcardOutlined';
+import Chat from '@mui/icons-material/Chat';
+import CheckCircle from '@mui/icons-material/CheckCircle';
+import CheckCircleRounded from '@mui/icons-material/CheckCircleRounded';
+import Clear from '@mui/icons-material/Clear';
+import ClearRounded from '@mui/icons-material/ClearRounded';
+import Close from '@mui/icons-material/Close';
+import CreateRounded from '@mui/icons-material/CreateRounded';
+import DashboardCustomizeOutlined from '@mui/icons-material/DashboardCustomizeOutlined';
+import Delete from '@mui/icons-material/Delete';
+import DragIndicator from '@mui/icons-material/DragIndicator';
+import EditOutlined from '@mui/icons-material/EditOutlined';
+import ErrorRounded from '@mui/icons-material/ErrorRounded';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import ExpandMoreOutlined from '@mui/icons-material/ExpandMoreOutlined';
+import Facebook from '@mui/icons-material/Facebook';
+import FacebookRounded from '@mui/icons-material/FacebookRounded';
+import FavoriteBorderOutlined from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteRounded from '@mui/icons-material/FavoriteRounded';
+import FileCopy from '@mui/icons-material/FileCopy';
+import FilterListRounded from '@mui/icons-material/FilterListRounded';
+import HelpOutlineOutlined from '@mui/icons-material/HelpOutlineOutlined';
+import HomeOutlined from '@mui/icons-material/HomeOutlined';
+import KeyboardArrowDownRounded from '@mui/icons-material/KeyboardArrowDownRounded';
+import LabelRounded from '@mui/icons-material/LabelRounded';
+import Memory from '@mui/icons-material/Memory';
+import MobileFriendly from '@mui/icons-material/MobileFriendly';
+import NotificationsNoneOutlined from '@mui/icons-material/NotificationsNoneOutlined';
+import PanToolOutlined from '@mui/icons-material/PanToolOutlined';
+import PhotoCameraOutlined from '@mui/icons-material/PhotoCameraOutlined';
+import Refresh from '@mui/icons-material/Refresh';
+import RemoveRounded from '@mui/icons-material/RemoveRounded';
+import RestoreRounded from '@mui/icons-material/RestoreRounded';
+import Save from '@mui/icons-material/Save';
+import SaveOutlined from '@mui/icons-material/SaveOutlined';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import Share from '@mui/icons-material/Share';
+import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined';
+import SmsFailedOutlined from '@mui/icons-material/SmsFailedOutlined';
+import StarOutlineRounded from '@mui/icons-material/StarOutlineRounded';
+import StarOutlined from '@mui/icons-material/StarOutlined';
+import StarRounded from '@mui/icons-material/StarRounded';
+import SyncAlt from '@mui/icons-material/SyncAlt';
+import ThumbDownOutlined from '@mui/icons-material/ThumbDownOutlined';
+import ThumbUpOffAltOutlined from '@mui/icons-material/ThumbUpOffAltOutlined';
+import ThumbUpOutlined from '@mui/icons-material/ThumbUpOutlined';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import WarningRounded from '@mui/icons-material/WarningRounded';
+import WrapText from '@mui/icons-material/WrapText';
+import FreeBreakfastOutlined from '@mui/icons-material/FreeBreakfastOutlined';
+import PlayCircleOutlined from '@mui/icons-material/PlayCircleOutlined';
+import LiveHelpOutlined from '@mui/icons-material/LiveHelpOutlined';
+import CodeRounded from '@mui/icons-material/CodeRounded';
+import PictureAsPdfOutlined from '@mui/icons-material/PictureAsPdfOutlined';
+import TextSnippetOutlined from '@mui/icons-material/TextSnippetOutlined';
+import AudiotrackOutlined from '@mui/icons-material/AudiotrackOutlined';
+import CloudDownloadOutlined from '@mui/icons-material/CloudDownloadOutlined';
+import CoPresentOutlined from '@mui/icons-material/CoPresentOutlined';
+import MmsOutlined from '@mui/icons-material/MmsOutlined';
+import LiveTvOutlined from '@mui/icons-material/LiveTvOutlined';
+import QuizOutlined from '@mui/icons-material/QuizOutlined';
+import MoodOutlined from '@mui/icons-material/MoodOutlined';
+import AssignmentOutlined from '@mui/icons-material/AssignmentOutlined';
+import AssignmentTurnedInOutlined from '@mui/icons-material/AssignmentTurnedInOutlined';
+import HelpOutlineRounded from '@mui/icons-material/HelpOutlineRounded';
+import FeedbackOutlined from '@mui/icons-material/FeedbackOutlined';
+import FormatSize from '@mui/icons-material/FormatSize';
+import FolderOutlined from '@mui/icons-material/FolderOutlined';
+
+const IconName = {
+    FolderOutlined: FolderOutlined,
+    FormatSize: FormatSize,
+    FeedbackOutlined: FeedbackOutlined,
+    HelpOutlineRounded: HelpOutlineRounded,
+    AssignmentTurnedInOutlined: AssignmentTurnedInOutlined,
+    AssignmentOutlined: AssignmentOutlined,
+    MoodOutlined: MoodOutlined,
+    QuizOutlined: QuizOutlined,
+    LiveTvOutlined: LiveTvOutlined,
+    MmsOutlined: MmsOutlined,
+    CoPresentOutlined: CoPresentOutlined,
+    CloudDownloadOutlined: CloudDownloadOutlined,
+    AudiotrackOutlined: AudiotrackOutlined,
+    TextSnippetOutlined: TextSnippetOutlined,
+    PictureAsPdfOutlined: PictureAsPdfOutlined,
+    CodeRounded: CodeRounded,
+    LiveHelpOutlined: LiveHelpOutlined,
+    PlayCircleOutlined: PlayCircleOutlined,
+    FreeBreakfastOutlined: FreeBreakfastOutlined,
+    Facebook: Facebook,
+    HomeOutlined: HomeOutlined,
+    SyncAlt: SyncAlt,
+    ArrowRight: ArrowRight,
+    ThumbUpOutlined: ThumbUpOutlined,
+    Bookmark: Bookmark,
+    BookmarkBorder: BookmarkBorder,
+    ArrowForwardRounded: ArrowForwardRounded,
+    ClearRounded: ClearRounded,
+    Close: Close,
+    CheckCircleRounded: CheckCircleRounded,
+    WarningRounded: WarningRounded,
+    ErrorRounded: ErrorRounded,
+    ExpandMore: ExpandMore,
+    CheckCircle: CheckCircle,
+    DashboardCustomizeOutlined: DashboardCustomizeOutlined,
+    AutoAwesomeMosaicOutlined: AutoAwesomeMosaicOutlined,
+    HelpOutlineOutlined: HelpOutlineOutlined,
+    SmsFailedOutlined: SmsFailedOutlined,
+    ArrowBackOutlined: ArrowBackOutlined,
+    AutoFixHigh: AutoFixHigh,
+    WrapText: WrapText,
+    ThumbDownOutlined: ThumbDownOutlined,
+    KeyboardArrowDownRounded: KeyboardArrowDownRounded,
+    ArrowDropDownOutlined: ArrowDropDownOutlined,
+    ArrowDropUpOutlined: ArrowDropUpOutlined,
+    ArrowDropUp: ArrowDropUp,
+    ArrowDropDown: ArrowDropDown,
+    StarOutlined: StarOutlined,
+    ThumbUpOffAltOutlined: ThumbUpOffAltOutlined,
+    MobileFriendly: MobileFriendly,
+    AccountTreeOutlined: AccountTreeOutlined,
+    Cached: Cached,
+    ArrowBackRounded: ArrowBackRounded,
+    ShoppingCartOutlined: ShoppingCartOutlined,
+    PanToolOutlined: PanToolOutlined,
+    CardGiftcardOutlined: CardGiftcardOutlined,
+    Apple: Apple,
+    Save: Save,
+    ArrowDownwardRounded: ArrowDownwardRounded,
+    ArrowUpwardRounded: ArrowUpwardRounded,
+    Chat: Chat,
+    AssignmentIndOutlined: AssignmentIndOutlined,
+    CreateRounded: CreateRounded,
+    PhotoCameraOutlined: PhotoCameraOutlined,
+    SaveOutlined: SaveOutlined,
+    FavoriteRounded: FavoriteRounded,
+    SearchOutlined: SearchOutlined,
+    AccessTimeOutlined: AccessTimeOutlined,
+
+    ExpandMoreOutlined: ExpandMoreOutlined,
+    Add: Add,
+    DragIndicator: DragIndicator,
+    VisibilityOff: VisibilityOff,
+    Visibility: Visibility,
+    Refresh: Refresh,
+    AddRounded: AddRounded,
+    RemoveRounded: RemoveRounded,
+    Clear: Clear,
+    Delete: Delete,
+    RestoreRounded: RestoreRounded,
+    FileCopy: FileCopy,
+    Memory: Memory,
+    AddToPhotosRounded: AddToPhotosRounded,
+    LabelRounded: LabelRounded,
+    StarRounded: StarRounded,
+    NotificationsNoneOutlined: NotificationsNoneOutlined,
+    StarOutlineRounded: StarOutlineRounded,
+    FavoriteBorderOutlined: FavoriteBorderOutlined,
+    Share: Share,
+    EditOutlined: EditOutlined,
+    FilterListRounded: FilterListRounded,
+    MoreVert: MoreVert,
+    OndemandVideoOutlined: OndemandVideoOutlined,
+    MessageOutlined: MessageOutlined,
+    EqualizerRounded: EqualizerRounded,
+    BookmarksOutlined: BookmarksOutlined,
+    PriorityHighRounded: PriorityHighRounded,
+    MoreHorizRounded: MoreHorizRounded,
+    Star: Star,
+    CheckRounded: CheckRounded,
+    OutlinedFlagRounded: OutlinedFlagRounded,
+    HistoryOutlined: HistoryOutlined,
+    FeedOutlined: FeedOutlined,
+    HistoryRounded: HistoryRounded,
+    CardGiftcardRounded: CardGiftcardRounded,
+    Brightness2Outlined: Brightness2Outlined,
+    SchoolOutlined: SchoolOutlined,
+    FileDownloadOutlined: FileDownloadOutlined,
+    ReportGmailerrorredRounded: ReportGmailerrorredRounded,
+    LinkedIn: LinkedIn,
+    YouTube: YouTube,
+    GitHub: GitHub,
+    Twitter: Twitter,
+    FacebookRounded: FacebookRounded,
 }
 
 export interface IconProps {
@@ -20,34 +247,22 @@ export default React.memo(React.forwardRef(function Icon({ icon, iconBackup, typ
         } /></SvgIcon>
     }
 
-    try {
-        if (icon && type === 'material' && typeof icon === 'string') {
-            //eslint-disable-next-line
-            let resolved = require(`@mui/icons-material/esm/${icon}`).default;
-            return React.createElement(resolved, { ...rest, ref: ref });
-        }
+    if (icon === 'empty') {
+        return null;
+    }
 
-        if (iconBackup && type === 'material' && typeof iconBackup === 'string') {
-            //eslint-disable-next-line
-            let resolved = require(`@mui/icons-material/esm/${iconBackup}`).default;
-            return React.createElement(resolved, { ...rest, ref: ref });
+    if (typeof icon === 'string') {
+        if (IconName[icon]) {
+            return React.createElement(IconName[icon], { ...rest, ref: ref });
         }
-
-    } catch (error) {
-
-        if (iconBackup) {
-            try {
-                //eslint-disable-next-line
-                let resolved = require(`@mui/icons-material/esm/${iconBackup}`).default;
-                return React.createElement(resolved, { ...rest, ref: ref });
-            } catch (error) {
-                //
-            }
-        }
-        //
     }
 
     return null;
 }), (props1, props2) => {
     return props1.renderVersion === props2.renderVersion && props1.icon === props2.icon;
 });
+
+export type IconFormat = keyof typeof IconName | 'empty'
+    | {
+        custom: string
+    }
