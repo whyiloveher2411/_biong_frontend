348486a932bc4d10dc30f35a504f27fca0771e0d
ken <quandang@poccupine.com>
2023-03-28 18:20:48 +0700

    test optimize icon

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,4 +1,3 @@
-import SvgIcon from 'components/atoms/SvgIcon';
 import React from 'react'
 
 export type IconFormat = string | {
@@ -14,39 +13,6 @@ export interface IconProps {
 }
 
 export default React.memo(React.forwardRef(function Icon({ icon, iconBackup, type = 'material', renderVersion, ...rest }: IconProps, ref: React.ForwardedRef<ANY>) {
-
-    if (icon && typeof icon === 'object' && icon.custom) {
-        return <SvgIcon ref={ref} {...rest}> <svg dangerouslySetInnerHTML={{ __html: icon.custom }
-        } /></SvgIcon>
-    }
-
-    try {
-        if (icon && type === 'material' && typeof icon === 'string') {
-            //eslint-disable-next-line
-            let resolved = require(`@mui/icons-material/esm/${icon}`).default;
-            return React.createElement(resolved, { ...rest, ref: ref });
-        }
-
-        if (iconBackup && type === 'material' && typeof iconBackup === 'string') {
-            //eslint-disable-next-line
-            let resolved = require(`@mui/icons-material/esm/${iconBackup}`).default;
-            return React.createElement(resolved, { ...rest, ref: ref });
-        }
-
-    } catch (error) {
-
-        if (iconBackup) {
-            try {
-                //eslint-disable-next-line
-                let resolved = require(`@mui/icons-material/esm/${iconBackup}`).default;
-                return React.createElement(resolved, { ...rest, ref: ref });
-            } catch (error) {
-                //
-            }
-        }
-        //
-    }
-
     return null;
 }), (props1, props2) => {
     return props1.renderVersion === props2.renderVersion && props1.icon === props2.icon;
