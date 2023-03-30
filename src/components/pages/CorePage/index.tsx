656465fe42997a962dea807e87e9bc2c3cc9a019
ken <quandang@poccupine.com>
2023-03-30 15:05:07 +0700

    test optimize lazy

--- src/components/pages/CorePage/index.tsx
+++ src/components/pages/CorePage/index.tsx
@@ -5,6 +5,7 @@ import { toCamelCase } from 'helpers/string';
 import React, { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 
+
 // const templateMap = {
 //     Homepage: React.lazy(() => import("./HomePage/index")),
 //     About: React.lazy(() => import("./About/index")),
@@ -24,17 +25,19 @@ import { useParams } from 'react-router-dom';
 
 function CorePage() {
 
-    const oldComponent = React.useRef<React.ReactNode>(<Box sx={{ height: '300vh' }} />);
+    const done = React.useRef<{
+        [key: string]: React.ReactNode
+    }>({
 
-    let { page, tab, subtab1, subtab2 } = useParams<{
-        page: string,
-        tab: string,
-        subtab1: string,
-        subtab2: string,
-    }>();
+    });
 
-    return <Suspense fallback={oldComponent.current}>
-        {(() => {
+    const [ComponentNew, setComponentNew] = React.useState<ANY>(null);
+
+    const { page, tab, subtab1, subtab2 } = useParams();
+
+    React.useEffect(() => {
+        if (!done.current[page + '1']) {
+            setComponentNew(<Box />);
             const imported = async () => {
                 if (page) {
 
@@ -105,10 +108,32 @@ function CorePage() {
 
                 return import("./HomePage/index");
             };
+            const Component = imported();
+            setComponentNew(Component);
 
+            if (Component.then) {
+                Component.then(C => {
+                    if (C.default) {
+                        done.current[page + '1'] = React.createElement(C.default, {});
+                    }
+                });
+            }
+        }
+    }, [page, tab, subtab1, subtab2]);
 
-            const Component = React.lazy(() => imported());
-            return <Component />
+    if (done.current[page + '1']) {
+        return <> {done.current[page + '1']} </>
+    }
+
+    return <Suspense>
+        {(() => {
+            if (ComponentNew) {
+                const Component = React.lazy(() => ComponentNew);
+                return <Component />
+            } else {
+                const Component = React.lazy(() => import("./Error404"));
+                return <Component />
+            }
         })()}
     </Suspense>
 
