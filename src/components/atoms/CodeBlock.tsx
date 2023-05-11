684b35a9196b4dd27d050c7ab0f7b9becf556852
ken <quandang@poccupine.com>
2023-05-11 12:32:09 +0700

    add title to copy code

--- src/components/atoms/CodeBlock.tsx
+++ src/components/atoms/CodeBlock.tsx
@@ -17,7 +17,7 @@ const CodeBlock = React.forwardRef(({ html, sx, ...rest }: BoxProps & { html: st
                 button.classList.add('btnCopyCode');
 
                 button.innerHTML = '<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ContentCopyOutlinedIcon"><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path></svg>';
-
+                button.title = 'Copy Code';
                 button.addEventListener('click', function () {
                     const content = pre.querySelector('code')?.textContent;
                     if (content) {
