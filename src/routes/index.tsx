ae9f2e6e5cc24c78490dd1d9b9b861bbbc2b80b3
David Dang <dangthuyenquan@gmail.com>
2024-11-09 15:43:53 +0700

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -76,7 +76,7 @@ function Router() {
             <Box sx={(theme) => ({
                 paddingTop: layoutState.headerVisible ? '64px' : 0,
                 minHeight: layoutState.headerVisible ? `calc( 100vh - ${64 + 64}px )` : '100vh',
-                backgroundColor: theme.palette.mode === 'light' && layoutState.isIframeOauth ? 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)' : 'divider'
+                backgroundColor: theme.palette.mode === 'light' ? layoutState.isIframeOauth ? 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)' : '#f2f4f7' : '#1c1c1d'
             })}>
                 <Routes>
                     {
